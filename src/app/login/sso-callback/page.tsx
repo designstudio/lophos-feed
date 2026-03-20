05d09b75f9d201ee9d80ead6429860d8a8665a01
Henrique <henriquebignatti@gmail.com>
2026-03-19 21:49:44 -0300

    feat: adiciona rota sso-callback para OAuth Google

---
+++ src/app/login/sso-callback/page.tsx
@@ -0,0 +1,8 @@
+'use client'
+import { AuthenticateWithRedirectCallback } from '@clerk/nextjs'
+
+export const dynamic = 'force-dynamic'
+
+export default function SSOCallbackPage() {
+  return <AuthenticateWithRedirectCallback />
+}
