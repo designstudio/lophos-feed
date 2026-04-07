418362b135d972c1bea7766a679424c1f79835d2
Henrique <henriquebignatti@gmail.com>
2026-04-07 20:22:19 -0300

    Reduce sidebar gap to bring content closer

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -407,7 +407,7 @@ export function Sidebar({ onRefresh, refreshing, refreshLabel, refreshTitle }: P
     <div>
       {!isReady && (
         <aside
-          className="my-4 ml-4 mr-4 flex flex-shrink-0 flex-col overflow-hidden rounded-[1.5rem] border border-border bg-bg-primary shadow-[0_12px_40px_rgba(17,17,17,0.05)]"
+          className="my-4 ml-4 mr-2 flex flex-shrink-0 flex-col overflow-hidden rounded-[1.5rem] border border-border bg-bg-primary shadow-[0_12px_40px_rgba(17,17,17,0.05)]"
           style={{
             width: resolvedWidth,
             height: 'calc(100dvh - 2rem)',
@@ -429,7 +429,7 @@ export function Sidebar({ onRefresh, refreshing, refreshLabel, refreshTitle }: P
       )}
 
       <aside
-        className="my-4 ml-4 mr-4 flex flex-shrink-0 flex-col overflow-hidden rounded-[1.5rem] border border-border bg-bg-primary shadow-[0_12px_40px_rgba(17,17,17,0.05)]"
+        className="my-4 ml-4 mr-2 flex flex-shrink-0 flex-col overflow-hidden rounded-[1.5rem] border border-border bg-bg-primary shadow-[0_12px_40px_rgba(17,17,17,0.05)]"
         style={{
           width: resolvedWidth,
           height: 'calc(100dvh - 2rem)',
